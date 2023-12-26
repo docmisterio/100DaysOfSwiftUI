04862343295b603c163b89298f9c0e1ff3016a00
Zach Becker <doc.misterio@gmail.com>
2023-12-26 14:49:41 -0600

    Adding ViewsAndModifiers app

---
+++ zach/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Zach Becker on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
