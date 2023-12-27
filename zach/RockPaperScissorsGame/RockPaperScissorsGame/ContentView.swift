e6b90a30a15eacff88d92a14d2982f690fd848df
Zach Becker <doc.misterio@gmail.com>
2023-12-26 22:28:20 -0600

    First commit for RockPaperScissors

---
+++ zach/RockPaperScissorsGame/RockPaperScissorsGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RockPaperScissorsGame
+//
+//  Created by Zach Becker on 12/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zach/RockPaperScissorsGame/RockPaperScissorsGame/RockPaperScissorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsGameApp.swift
+//  RockPaperScissorsGame
+//
+//  Created by Zach Becker on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
