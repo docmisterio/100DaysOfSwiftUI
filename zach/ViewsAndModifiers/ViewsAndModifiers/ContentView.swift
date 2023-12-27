9510cf22d3243866488a729767fec99dc6c8c726
Zach Becker <doc.misterio@gmail.com>
2023-12-26 19:51:13 -0600

    Adding the challenge for day 24

--- zach/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ zach/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -4,8 +4,23 @@
 //
 //  Created by Zach Becker on 12/26/23.
 //
+// Create a custom ViewModifier (and accompanying View extension) that makes a view have a large, blue font suitable for prominent titles in a view.
 
 import SwiftUI
+
+struct AlterText: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.blue)
+    }
+}
+
+extension View {
+    func alteredText() -> some View {
+        modifier(AlterText())
+    }
+}
 
 struct ContentView: View {
     var body: some View {
@@ -14,6 +29,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .alteredText()
         }
         .padding()
     }
