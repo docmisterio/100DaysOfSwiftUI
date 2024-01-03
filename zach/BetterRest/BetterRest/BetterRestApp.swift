29acbc5bb693d829c684a3b06bb3b8d2215c6137
Zach Becker <doc.misterio@gmail.com>
2024-01-02 18:38:42 -0600

    committing examples to go back to as I write this new app.

---
+++ zach/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Zach Becker on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ zach/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Zach Becker on 1/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
+    var body: some View {
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+        Text(Date.now, format: .dateTime.hour().minute())
+    }
+    
+    func exampleDates() {
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? .now
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+    }
+}
+
+#Preview {
+    ContentView()
+}
