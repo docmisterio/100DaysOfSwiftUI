44152612798942f935069e52628ad9fd2339bf4a
Zach Becker <doc.misterio@gmail.com>
2024-11-17 22:11:39 -0600

    locked in the first draft of the ScreenObjectModel.

--- zach/RockPaperScissorsGame/RockPaperScissorsGameUITests/XCUITestObjectModel.swift
+++ zach/RockPaperScissorsGame/RockPaperScissorsGameUITests/XCUITestObjectModel.swift
@@ -2,11 +2,40 @@ import Foundation
 import XCTest
 
 class XCUITestObjectModel: XCTestCase {
+    var yourScore: Int = 0
+    
     enum Buttons {
-        
+       static let rock = XCUIApplication().buttons["🪨"]
+        static let paper = XCUIApplication().buttons["📃"]
+        static let scissors = XCUIApplication().buttons["✂️"]
     }
     
     enum Labels {
-        
+        static let rockPaperScissorsTitle = XCUIApplication().staticTexts["Rock, Paper, Scissors"]
+        static let win = XCUIApplication().staticTexts["Win"]
+        static let lose = XCUIApplication().staticTexts["Lose"]
+        static let rock = XCUIApplication().staticTexts["🪨"]
+        static let paper = XCUIApplication().staticTexts["📃"]
+        static let scissors = XCUIApplication().staticTexts["✂️"]
     }
+    
+    enum Alerts {
+        static let tieTitle = XCUIApplication().alerts["Tie"]
+        static let WinTitle = XCUIApplication().alerts["Win"]
+        static let LoseTitle = XCUIApplication().alerts["Lose"]
+        
+        static func yourScoreAlert(score: Int) -> XCUIElement {
+                return XCUIApplication().alerts["Your Score is \(score)."]
+            }
+        
+        enum Buttons {
+            static let continueButton = XCUIApplication().buttons["Continue"]
+        }
+    }
+}
+
+enum RockPaperScissors: String, CaseIterable {
+    case rock = "🪨"
+    case paper = "📃"
+    case scissors = "✂️"
 }
