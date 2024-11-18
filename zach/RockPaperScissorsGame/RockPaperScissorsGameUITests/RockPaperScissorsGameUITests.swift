4d128889e1012fdfb459d1a25694f4f8ae54c19c
Zach Becker <doc.misterio@gmail.com>
2024-11-17 22:10:49 -0600

    cleaning up the test file before doing the bulk of the work in this file.

--- zach/RockPaperScissorsGame/RockPaperScissorsGameUITests/RockPaperScissorsGameUITests.swift
+++ zach/RockPaperScissorsGame/RockPaperScissorsGameUITests/RockPaperScissorsGameUITests.swift
@@ -1,10 +1,4 @@
-//
-//  RockPaperScissorsGameUITests.swift
-//  RockPaperScissorsGameUITests
-//
-//  Created by Zach Becker on 11/17/24.
-//
-
+import Foundation
 import XCTest
 
 final class RockPaperScissorsGameUITests: XCTestCase {
@@ -16,16 +10,12 @@ final class RockPaperScissorsGameUITests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+       
     }
 
-    @MainActor
+//    @MainActor
     func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+     
     }
 
     @MainActor
