a70b4aba3db91f29cbb7ce4a524b83a8db1f68c6
Zach Becker <doc.misterio@gmail.com>
2024-11-20 18:41:49 -0600

    added testValidateTitle and testGame as both static and dynamic testing stratagies in random apps

--- zach/RockPaperScissorsGame/RockPaperScissorsGameUITests/RockPaperScissorsGameUITests.swift
+++ zach/RockPaperScissorsGame/RockPaperScissorsGameUITests/RockPaperScissorsGameUITests.swift
@@ -3,27 +3,35 @@ import XCTest
 
 final class RockPaperScissorsGameUITests: XCTestCase {
     var app = XCUIApplication()
-
+    
     override func setUpWithError() throws {
         app.launch()
         continueAfterFailure = false
     }
-
+    
     override func tearDownWithError() throws {
-       
+        /// Nothing here yet.
     }
-
-//    @MainActor
-    func testExample() throws {
-     
-    }
-
+    
     @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
+    func testValidateTitle() throws {
+        verifyElement(XCUITestObjectModel.Labels.rockPaperScissorsTitle)
+    }
+    
+    func testGame() throws {
+        if XCUITestObjectModel.Labels.win.exists {
+            winWinScenario()
+        } else {
+            loseWinScenario()
+        }
+        
+        @MainActor
+        func testLaunchPerformance() throws {
+            if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+                // This measures how long it takes to launch your application.
+                measure(metrics: [XCTApplicationLaunchMetric()]) {
+                    XCUIApplication().launch()
+                }
             }
         }
     }
