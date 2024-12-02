50224b49a5ad95935042d01031ea047763e43cfc
Zach Becker <doc.misterio@gmail.com>
2024-12-01 22:47:10 -0600

    Adding a TaskListItem.swift to handle the item behavoir. hopefully this is a good idea later.

--- zach/StardewToDo/StardewToDo/TaskListItem.swift
+++ zach/StardewToDo/StardewToDo/TaskListItem.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct TaskListItem: View {
+    @Binding var task: Task
+    var onComplete: () -> Void
+
+    var body: some View {
+        HStack {
+            Text(task.title)
+                .strikethrough(task.isCompleted)
+            Spacer()
+            if task.isCompleted {
+                Image(systemName: "checkmark.circle.fill")
+                    .foregroundColor(.green)
+            } else {
+                Button(action: {
+                    onComplete()
+                }) {
+                    Image(systemName: "circle")
+                }
+                .buttonStyle(BorderlessButtonStyle())
+            }
+        }
+    }
+}
+
